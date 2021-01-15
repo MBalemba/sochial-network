8442e15847c358196ec22ab691353828c99fc197
Balemba <balembamaks@gmail.com>
2021-01-15 15:49:53 +0300

    Lesson 37

--- src/index.js
+++ src/index.js
@@ -2,26 +2,26 @@ import React from 'react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {states, subscribe} from "./redux/state";
 import ReactDOM from 'react-dom';
 import './index.css';
-import data, {updateNewText} from "./redux/state";
+import store from "./redux/state";
 
 
 
-let renderEntireTree = () => {
+
+let renderEntireTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
-            <App data = {data} func = {states} updateNewText = {updateNewText}/>
+            <App data = {store.getData()} func = {store.addPost.bind(store)} updateNewText = {store.updateNewText.bind(store)}/>
         </React.StrictMode>,
         document.getElementById('root')
     );
 }
 
 
-renderEntireTree(states);
+renderEntireTree(store.getData());
 
-subscribe(renderEntireTree);
+store.subscribe(renderEntireTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,93 +1,100 @@
 
 
-let renderEntireTree = () => {
-    console.log('state was changed')
-};
+let store = {
+    _data: {
 
-let data = {
-
-    profilePage: {
-        posts: [
-            {
-                name: "Макс Балемба",
-                id: "0",
-                srck: "https://sun1-92.userapi.com/impg/Aa9d2TJGoAIu89Q-RpSDKreTzPlDXkPaAZdL7A/LcFwPJVaTYA.jpg?size=100x0&quality=96&crop=558,189,953,953&sign=f4f26965a68cafa07dab7462ccfbb34e&ava=1",
-                messege: `sdsdsdsdsdsdsdsdsdsd
+        profilePage: {
+            posts: [
+                {
+                    name: "Макс Балемба",
+                    id: "0",
+                    srck: "https://sun1-92.userapi.com/impg/Aa9d2TJGoAIu89Q-RpSDKreTzPlDXkPaAZdL7A/LcFwPJVaTYA.jpg?size=100x0&quality=96&crop=558,189,953,953&sign=f4f26965a68cafa07dab7462ccfbb34e&ava=1",
+                    messege: `sdsdsdsdsdsdsdsdsdsd
                 sdsdsddddddddddddddddddd
                 sdsdssssssssdsdsdsd`
 
-            },
-            {
-                name: "Владимир Никольский",
-                id: "1",
-                srck: "https://sun1-24.userapi.com/impg/SCePpqVJ_bHX7JrqMzgghJT3zj6XygsBLN8NOQ/WstJf3YaLJg.jpg?size=100x0&quality=96&crop=391,0,856,856&sign=5170b4f5f2e39eb32a29485962779c7f&ava=1",
-                messege: 'Hello'
-            },
-            {
-                name: "Денис Золотарев",
-                id: "2",
-                srck: "https://sun1-83.userapi.com/impg/W9sX2s5z-8-HwEXFfUIKTQWYy6u_EDhBH8g3rw/aW_Mgq_G_Ls.jpg?size=100x0&quality=96&crop=3,227,712,712&sign=b6b9ecdcaad12a005dec078b7c32ad6f&ava=1",
-                messege: 'Hello'
-            },
-            {
-                name: "Артур Амосов",
-                id: "3",
-                srck: "https://sun1-98.userapi.com/impg/c858436/v858436975/10a9b4/lPJp4Rbly2k.jpg?size=100x0&quality=96&crop=1,704,1120,1120&sign=fbbc356ede90a909b5e98b79b75efaad&ava=1",
-                messege: 'Hello'
-            },
+                },
+                {
+                    name: "Владимир Никольский",
+                    id: "1",
+                    srck: "https://sun1-24.userapi.com/impg/SCePpqVJ_bHX7JrqMzgghJT3zj6XygsBLN8NOQ/WstJf3YaLJg.jpg?size=100x0&quality=96&crop=391,0,856,856&sign=5170b4f5f2e39eb32a29485962779c7f&ava=1",
+                    messege: 'Hello'
+                },
+                {
+                    name: "Денис Золотарев",
+                    id: "2",
+                    srck: "https://sun1-83.userapi.com/impg/W9sX2s5z-8-HwEXFfUIKTQWYy6u_EDhBH8g3rw/aW_Mgq_G_Ls.jpg?size=100x0&quality=96&crop=3,227,712,712&sign=b6b9ecdcaad12a005dec078b7c32ad6f&ava=1",
+                    messege: 'Hello'
+                },
+                {
+                    name: "Артур Амосов",
+                    id: "3",
+                    srck: "https://sun1-98.userapi.com/impg/c858436/v858436975/10a9b4/lPJp4Rbly2k.jpg?size=100x0&quality=96&crop=1,704,1120,1120&sign=fbbc356ede90a909b5e98b79b75efaad&ava=1",
+                    messege: 'Hello'
+                },
             ],
-        newPostText: 'post write now...'
+            newPostText: 'post write now...'
+        },
+
+        dialogPage: {
+            blockMessege: [
+                {
+                    name: "Макс Балемба",
+                    id: "0",
+                    srck: "https://sun1-92.userapi.com/impg/Aa9d2TJGoAIu89Q-RpSDKreTzPlDXkPaAZdL7A/LcFwPJVaTYA.jpg?size=100x0&quality=96&crop=558,189,953,953&sign=f4f26965a68cafa07dab7462ccfbb34e&ava=1",
+
+                },
+                {
+                    name: "Владимир Никольский",
+                    id: "1",
+                    srck: "https://sun1-24.userapi.com/impg/SCePpqVJ_bHX7JrqMzgghJT3zj6XygsBLN8NOQ/WstJf3YaLJg.jpg?size=100x0&quality=96&crop=391,0,856,856&sign=5170b4f5f2e39eb32a29485962779c7f&ava=1",
+                },
+                {
+                    name: "Денис Золотарев",
+                    id: "2",
+                    srck: "https://sun1-83.userapi.com/impg/W9sX2s5z-8-HwEXFfUIKTQWYy6u_EDhBH8g3rw/aW_Mgq_G_Ls.jpg?size=100x0&quality=96&crop=3,227,712,712&sign=b6b9ecdcaad12a005dec078b7c32ad6f&ava=1",
+                },
+                {
+                    name: "Артур Амосов",
+                    id: "3",
+                    srck: "https://sun1-98.userapi.com/impg/c858436/v858436975/10a9b4/lPJp4Rbly2k.jpg?size=100x0&quality=96&crop=1,704,1120,1120&sign=fbbc356ede90a909b5e98b79b75efaad&ava=1",
+                }
+            ],
+        },
+
     },
-
-    dialogPage: {
-        blockMessege: [
-            {
-                name: "Макс Балемба",
-                id: "0",
-                srck: "https://sun1-92.userapi.com/impg/Aa9d2TJGoAIu89Q-RpSDKreTzPlDXkPaAZdL7A/LcFwPJVaTYA.jpg?size=100x0&quality=96&crop=558,189,953,953&sign=f4f26965a68cafa07dab7462ccfbb34e&ava=1",
-
-            },
-            {
-                name: "Владимир Никольский",
-                id: "1",
-                srck: "https://sun1-24.userapi.com/impg/SCePpqVJ_bHX7JrqMzgghJT3zj6XygsBLN8NOQ/WstJf3YaLJg.jpg?size=100x0&quality=96&crop=391,0,856,856&sign=5170b4f5f2e39eb32a29485962779c7f&ava=1",
-            },
-            {
-                name: "Денис Золотарев",
-                id: "2",
-                srck: "https://sun1-83.userapi.com/impg/W9sX2s5z-8-HwEXFfUIKTQWYy6u_EDhBH8g3rw/aW_Mgq_G_Ls.jpg?size=100x0&quality=96&crop=3,227,712,712&sign=b6b9ecdcaad12a005dec078b7c32ad6f&ava=1",
-            },
-            {
-                name: "Артур Амосов",
-                id: "3",
-                srck: "https://sun1-98.userapi.com/impg/c858436/v858436975/10a9b4/lPJp4Rbly2k.jpg?size=100x0&quality=96&crop=1,704,1120,1120&sign=fbbc356ede90a909b5e98b79b75efaad&ava=1",
-            }
-        ],
+    getData(){
+      return this._data
     },
+    _callSubscriber() {
+        console.log('state was changed')
+    },
+    addPost() {
+        debugger;
+        let newPost = {
+            name: "Макс Балемба",
+            id: "0",
+            srck: "https://sun1-92.userapi.com/impg/Aa9d2TJGoAIu89Q-RpSDKreTzPlDXkPaAZdL7A/LcFwPJVaTYA.jpg?size=100x0&quality=96&crop=558,189,953,953&sign=f4f26965a68cafa07dab7462ccfbb34e&ava=1",
+            messege: this._data.profilePage.newPostText,
+        }
+        this._data.profilePage.posts.push(newPost);
+        this._data.profilePage.newPostText = '';
+        this._callSubscriber();
+    },
+    updateNewText (newText){
 
-}
-
-export let states = () =>{
-    let newPost = {
-        name: "Макс Балемба",
-        id: "0",
-        srck: "https://sun1-92.userapi.com/impg/Aa9d2TJGoAIu89Q-RpSDKreTzPlDXkPaAZdL7A/LcFwPJVaTYA.jpg?size=100x0&quality=96&crop=558,189,953,953&sign=f4f26965a68cafa07dab7462ccfbb34e&ava=1",
-        messege: data.profilePage.newPostText,
+        this._data.profilePage.newPostText = newText;
+        this._callSubscriber();
+    },
+    subscribe(observer){
+        this._callSubscriber = observer;
     }
-    data.profilePage.posts.push(newPost);
-    data.profilePage.newPostText = '';
-    renderEntireTree()
+    //Переопределяет _callSubscriber,
+    // присваивая значение функции, которая отрисует компоненты
 }
 
-export let updateNewText = (newText) =>{
 
-    data.profilePage.newPostText = newText;
-    renderEntireTree();
-}
 
-export const subscribe = (observer) => {
-    renderEntireTree = observer;
-}
+export default store
 
-export default data;
 
